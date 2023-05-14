668bd5a8468298d1792716111be0f9127da4b9df
Aditya Danturthi <aditya2096@gmail.com>
2023-05-14 17:18:29 -0230

    removed center align

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 
 function Content(){
   return(
-    <div className='flex flex-col items-start justify-start h-screen w-screen px-4 py-10'>
+    <div className='h-screen w-screen px-4 py-10'>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" 
-    alt="React logo" className="h-6 w-7" />
-      <h1 className='font-extrabold text-xl py-5'>Fun facts about React</h1>
-      <ul className="list-disc text-lg font-medium flex flex-col items-start justify-start px-6">
+    alt="React logo" className="h-8 w-9" />
+      <h1 className='font-extrabold text-3xl py-5'>Fun facts about React</h1>
+      <ul className="list-disc text-xl font-medium px-6">
         <li>Was first released in 2013</li>
         <li>Has over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
