43764b38b5463151b1d61f015ec75f1e8f866b46
Aditya Danturthi <aditya2096@gmail.com>
2023-05-14 16:53:52 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 
 function Content(){
   return(
-    <div className='flex flex-col items-start justify-start p-16 h-screen w-screen'>
+    <div className='flex flex-col items-start justify-start h-screen w-screen px-4 py-10'>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" 
-    alt="React logo" className="h-20 w-25" />
+    alt="React logo" className="h-6 w-7" />
       <h1 className='font-extrabold text-xl py-5'>Fun facts about React</h1>
-      <ul className="list-disc ms-0 px-16 text-lg font-medium py-5 m-0 flex flex-col items-start justify-start">
+      <ul className="list-disc text-lg font-medium flex flex-col items-start justify-start px-6">
         <li>Was first released in 2013</li>
         <li>Has over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
