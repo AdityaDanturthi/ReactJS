11e93b041114232ce44ff055d671f4d11fdda4eb
Aditya Danturthi <aditya2096@gmail.com>
2023-05-14 08:53:10 -0230

    Fun facts about react

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Content />
     </div>
   );
 }
 
-function NavBar(){
+function Content(){
   return(
-    <nav className='flex flex-col items-start p-2'>
-      <h1 className='font-bold text-2xl'>Cafe</h1>
-      <ul className="list-disc list-inside">
-        <li>About</li>
-        <li>Pricing</li>
-        <li>Contact</li>
+    <div className='flex flex-col items-start justify-start p-16'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" 
+    alt="React logo" className="h-20 w-25" />
+      <h1 className='font-extrabold text-7xl py-5'>Fun facts about React</h1>
+      <ul className="list-disc ms-0 px-16 text-4xl font-medium py-5 m-0 flex flex-col items-start justify-start">
+        <li>Was first released in 2013</li>
+        <li>Has over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </nav>
+    </div>
   );
 }
 export default App;
